a8dc9bcce3fda3c56244d7ae8eb24b326692f295
SpieringsAE <maud_spierings@hotmail.com>
2024-07-08 16:52:56 +0200

    added get_static_ip, changed return types to dicts

--- ethernet.py
+++ ethernet.py
@@ -2,53 +2,69 @@ import subprocess
 import netifaces as ni
 import ipaddress
 
-def get_ethernet_mode() -> str:
+def get_ethernet_mode() -> dict:
 	stdout = subprocess.run(["nmcli", "-t", "con"], stdout=subprocess.PIPE, text=True)
 	result = stdout.stdout
 	result = result.split("\n")
-	mode = "unknown"
+	mode = {}
 	for name in result:
 		if "Wired connection static" in name:
 			if "eth0" in name:
-				mode = "static"
+				mode["mode"] = "static"
 				break
 		elif "Wired connection auto" in name:
 			if "eth0" in name:
-				mode = "auto"
+				mode["mode"] = "auto"
 				break
+	else: #if no break
+		mode["error"] = "Unknown mode"
 	return mode
 
-def set_ethernet_mode(mode: str) -> bool:
+def set_ethernet_mode(mode: str) -> dict:
 	if mode == "static":
 		subprocess.run(["nmcli", "con", "mod", "Wired connection auto", "connection.autoconnect", "no"])
 		subprocess.run(["nmcli", "con", "mod", "Wired connection static", "connection.autoconnect", "yes"])
 		subprocess.run(["nmcli", "con", "down", "Wired connection auto"])
 		subprocess.run(["nmcli", "con", "up", "Wired connection static"])
-		return True
+		return {"mode": "static"}
 	elif mode == "auto":
 		subprocess.run(["nmcli", "con", "mod", "Wired connection static", "connection.autoconnect", "no"])
 		subprocess.run(["nmcli", "con", "mod", "Wired connection auto", "connection.autoconnect", "yes"])
 		subprocess.run(["nmcli", "con", "down", "Wired connection static"])
 		subprocess.run(["nmcli", "con", "up", "Wired connection auto"])
-		return True
-	return False
+		return {"mode": "auto"}
+	return {"err": "Invalid mode, send either 'static' or 'auto'"}
 
-def set_static_ip(ip: str) -> str:
+def set_static_ip(ip: str) -> dict:
 	try:
-		ipaddress.ip_address(ip)
+		ipaddress.IPv4Address(ip)
 	except ValueError:
-		return "Error: Invalid IP"
+		return {"err": "Invalid IP"}
 	subprocess.run(["nmcli", "con", "mod", "Wired connection static", "ipv4.addresses", ip+"/16"])
 	if get_ethernet_mode() == "static":
 		subprocess.run(["nmcli", "con", "down", "Wired connection static"])
 		subprocess.run(["nmcli", "con", "up", "Wired connection static"])
-	return "Success"
+	return {"ip": ip}
 
-def get_static_ip() -> str:
-	pass
+def get_static_ip() -> dict:
+	output = subprocess.run(["nmcli", "-t", "con", "show", "Wired connection static"], stdout=subprocess.PIPE, text=True)
+	option = "ipv4.addresses:"
+	idx = output.stdout.find(option)
+	if idx >= 0:
+		ide = output.stdout.find("\n", idx + len(option))
+		if ide == idx + len(option):
+			return {"err": "No ip address configured"}
+		elif ide >= 0:
+			ip_ext = output.stdout[idx+len(option):ide]
+			ip = ip_ext.split("/")[0]
+			return {"ip": ip}
+		else:
+			return {"err": "Could get the ip address"}
+	else:
+		return {"err": "Could get the ip address"}
 
-def get_ethernet_ip() -> str:
+def get_ethernet_ip() -> dict:
 	try:
-		return ni.ifaddresses("eth0")[ni.AF_INET][0]["addr"]
+		return {"ip": ni.ifaddresses("eth0")[ni.AF_INET][0]["addr"]}
 	except:
-		return "no IP available"
+		return {"err": "no IP available"}
