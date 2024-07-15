c9b7ecbf29c9bb845d3a6bfc8a30fde3e3c561ef
SpieringsAE <maud_spierings@hotmail.com>
2024-07-15 16:53:30 +0200

    replaced copy paste html with a template

--- js/services.js
+++ js/services.js
@@ -1,43 +1,69 @@
+
+const services_list = [
+	["ssh", "SSH"],
+	["go-simulink", "Simulink"],
+	["nodered", "NodeRED"],
+	["go-bluetooth", "Bluetooth server"],
+	["go-upload-server", "Simulink upload server"],
+	["go-auto-shutdown", "Auto shutdown"],
+	["gadget-getty\@ttyGS0", "USB terminal"],
+	["getty\@ttymxc2", "Serial terminal"]
+]
+
 async function set_service(service) {
 	//get what the next state of wifi should be
 	let set_state = {};
-	if (!$("#"+service).prop("checked")) { //why does this work with a not?
+	if (!$("#service"+service).prop("checked")) { //why does this work with a not?
 		set_state.new_state = false;
 	} else {
 		set_state.new_state = true;
 	}
-	set_state.service = service;
+	set_state.service = services_list[service][0];
 	//try to make it a reality
 	try{
 		const resp = await post_json("/api/set_service", set_state);
 		if (resp.err != undefined) {
 			//server failed to toggle wifi
-			$('#'+service).prop("checked",!set_state.new_state);
-			alert("Could not toggle " + service + ":\n" + resp.err);
+			$('#service'+service).prop("checked",!set_state.new_state);
+			alert("Could not toggle " + services_list[service][0] + ":\n" + resp.err);
+		} else {
+			$('#service'+service).prop("checked",resp.new_state);
 		}
 	} catch(err) {
-		alert("Could not toggle " + service + ":\n" + err);
-		$('#'+service).prop("checked",!set_state.new_state);
+		alert("Could not toggle " + services_list[service][0] + ":\n" + err);
+		$('#service'+service).prop("checked",!set_state.new_state);
 	}
 }
 
-async function get_service(service) {
+async function get_service(service, index) {
 	try{
-		const resp = await post_json("/api/get_service", service);
-		if (resp.state) {
-			$('#'+service).prop("checked",true);
-		} else {
-			$('#'+service).prop("checked",false);
-		}
+		const resp = await post_json("/api/get_service", service[0]);
+		const service_templ = document.getElementById("service");
+		const services = document.getElementById("services");
+
+		const new_service = service_templ.content.cloneNode(true);
+		let p = new_service.querySelector("p");
+		p.textContent = service[1];
+		let cb = new_service.querySelector("input");
+		cb.id = "service" + index;
+		cb.setAttribute("onclick", "set_service("+index+")");
+		cb.setAttribute("checked", resp.state);
+		services.appendChild(new_service);
+		
 	} catch(err) {
-		alert("Could get the state of " + service + ":\n" + err);
+		alert("Could get the state of " + service[0] + ":\n" + err);
 	}
 }
 
 document.addEventListener('DOMContentLoaded', async function() {
-	const services = $(".service-cb");
-	for (var i = 0; i < services.length; i++) {
-		id = services[i].attributes.id.value;
-		get_service(id);
+	const services = document.getElementById("services");
+	services.textContent = ""
+	for (var i = 0; i < services_list.length; i++) {
+		get_service(services_list[i], i)
 	}
+	// const services = $(".service-cb");
+	// for (var i = 0; i < services.length; i++) {
+	// 	id = services[i].attributes.id.value;
+	// 	get_service(id);
+	// }
 }, false);
