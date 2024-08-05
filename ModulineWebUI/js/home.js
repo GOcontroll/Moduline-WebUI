17ac595ff4718ffadeae64f7555f9b96f6a66b45
SpieringsAE <maud_spierings@hotmail.com>
2024-08-05 13:25:39 +0200

    remove the full error from view

--- ModulineWebUI/js/home.js
+++ ModulineWebUI/js/home.js
@@ -5,25 +5,29 @@ document.addEventListener('DOMContentLoaded', async function() {
     const simulink_request = fetch('/api/get_sim_ver');
 	var res = await (await sn_request).json();
 	if (res.err) {
-		document.getElementById("serial_number").innerText = res.err;
+		document.getElementById("serial_number").innerText = "No controller serial number found";
+        console.log(res.err);
 	} else {
 		document.getElementById("serial_number").innerText = res.sn;
 	}
 	res = await (await software_request).json();
 	if (res.err) {
-		document.getElementById("controller_software").innerText = res.err;
+		document.getElementById("controller_software").innerText = "No controller software version found";
+        console.log(res.err);
 	} else {
 		document.getElementById("controller_software").innerText = res.version;
 	}
 	res = await (await hardware_request).json();
 	if (res.err) {
-		document.getElementById("controller_hardware").innerText = res.err;
+		document.getElementById("controller_hardware").innerText = "No controller hardware info found";
+        console.log(res.err);
 	} else {
 		document.getElementById("controller_hardware").innerText = res.hardware;
 	}
 	res = await (await simulink_request).json();
 	if (res.err) {
-		document.getElementById("simulink_version").innerText = res.err;
+		document.getElementById("simulink_version").innerText = "No simulink model version found";
+        console.log(res.err);
 	} else {
 		document.getElementById("simulink_version").innerText = res.version;
 	}
