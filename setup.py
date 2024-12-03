b84d9a496bf5ff1227763dcb9fce8e699ff495f3
Maud Spierings <maud_spierings@hotmail.com>
2024-12-03 09:01:50 +0100

    fix dependencies, now depends on PyModuline

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author="GOcontroll",
     author_email="info@gocontroll.com",
     maintainer="Maud Spierings",
-    install_requires=["microdot", "PyJWT", "netifaces"],
+    install_requires=["microdot", "PyJWT", "PyModuline"],
     packages=[
         "ModulineWebUI",
         "ModulineWebUI/assets",
